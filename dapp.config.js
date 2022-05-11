6d8e8e14d6206e50059b5968a53bbbf5eadf1620
mchimgee <104985677+mchimgee@users.noreply.github.com>
2022-05-11 23:26:06 +1000

    Add files via upload

--- dapp.config.js
+++ dapp.config.js
@@ -2,7 +2,7 @@
 const config = {
     title: 'BoredApe Dapp',
     description: 'A showcase dapp which is built for you to create your own',
-    contractAddress: '0x9c8cCcd9345768C104741F4742Cf1DE5f0f39F1A',
+    contractAddress: '0xFdbd2d3474B80a4492D95Fe70E2E15c9619f290b',
     maxMintAmount: 10,
     presaleMaxMintAmount:3,
     price: 0.01
